e31b60ed3be9fb4aabaf2ca9e9292cb97eced6db
ning.xiao <ning.xiao@nebutown.com>
2012-12-31 16:12:48 +0800

    Initial tvshow

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tvshow::Application.config.secret_token = 'b0e2921bad7d9b3d79930d44eb21246bf3ea2e2a1c7535217990170ee4d785e96e4c9425caa4018be95eaa0d9ff1151c0f54b3a05be8a95ad1b89bed1178348b'
