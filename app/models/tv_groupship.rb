254dde95ac3b0cb894c427411681e16e3b896b77
ning.xiao <ning.xiao@nebutown.com>
2013-01-03 00:35:21 +0800

    added the unique value

--- app/models/tv_groupship.rb
+++ app/models/tv_groupship.rb
@@ -1,5 +1,5 @@
 class TvGroupship < ActiveRecord::Base
   attr_accessible :tv_group_id, :tv_station_id
-  belongs_to :tv_station, :foreign_key => "tv_station_id"
-  belongs_to :tv_group, :foreign_key => "tv_group_id"
+  belongs_to :tv_station
+  belongs_to :tv_group
 end
--- app/models/tv_station.rb
+++ app/models/tv_station.rb
@@ -1,5 +1,5 @@
 class TvStation < ActiveRecord::Base
   attr_accessible :description, :image, :name
   has_many :tv_groupships, :dependent => :destroy
-  has_many :tv_groups, :through => :tv_groupships
+  has_many :tv_groups, :through => :tv_groupships, :uniq => true
 end
