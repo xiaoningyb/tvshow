09be2bedccb11e8de3201ec9d56724237a937587
ning.xiao <ning.xiao@nebutown.com>
2013-01-02 17:10:48 +0800

    added the change for model

--- app/models/tv_groupship.rb
+++ app/models/tv_groupship.rb
@@ -1,3 +1,5 @@
 class TvGroupship < ActiveRecord::Base
   attr_accessible :tv_group_id, :tv_station_id
+  belongs_to :tv_station
+  belongs_to :tv_group
 end
--- app/models/tv_station.rb
+++ app/models/tv_station.rb
@@ -1,3 +1,5 @@
 class TvStation < ActiveRecord::Base
   attr_accessible :description, :image, :name
+  has_many :tv_groupships
+  has_many :tv_groups, :through => :tv_groupships
 end
