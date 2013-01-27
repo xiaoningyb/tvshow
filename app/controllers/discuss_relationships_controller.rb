9da62437923b475a6d15ffaea6d03aae387d82e1
xiaoningyb <xiaoningyb@gmail.com>
2013-01-27 22:49:16 +0800

    added the reply message

--- app/controllers/discuss_relationships_controller.rb
+++ app/controllers/discuss_relationships_controller.rb
@@ -4,63 +4,65 @@ class DiscussRelationshipsController < ApplicationController
   #new discuss, it will be deprecated
   def new
     @discuss = Discuss.new(:user_id => current_user.id, :time => Time.now)
+    @src = nil
+    if params[:src] != nil
+      @src = Discuss.find(params[:src])
+    end
     @program = TvProgram.find(params[:program])
   end
 
   def create
     @discuss = Discuss.new(params[:discuss])
-    @quote = nil
-    if params[:quote] != nil
-      @quote = Discuss.find(params[:quote])
+    @src = nil
+    if params[:src] != nil
+      @src = Discuss.find(params[:src])
     end
     @program = TvProgram.find(params[:program_id])
-    DiscussRelationshipsController.create_relationship(current_user, @discuss, @quote, @program)
+    DiscussRelationshipsController.create_relationship(current_user, @src, @discuss, @program)
 
     redirect_to :controller => "tv_programs", :action => :show, :id => @program
   end
 
   def destory
     @discuss = Discuss.find(params[:src])
-    @quote = Discuss.find(params[:quote])
+    @source = Discuss.find(params[:quote])
     @program = TvProgram.find(params[:program])
-    DiscussRelationshipsController.destory_relationsip(@discuss, @quote, @program)
+    DiscussRelationshipsController.destory_relationsip(current_user, @src, @discuss, @program)
   end
 
-  def self.create_relationship(user, src, quote, program)
-    if user == nil || src == nil || program == nil
+  def self.create_relationship(user, source, discuss, program)
+    if user == nil || discuss == nil || program == nil
       return
     end
 
-    src.transaction do 
+    discuss.transaction do 
       user.update_attributes(:discuss_count => user.discuss_count.next)
-      src.update_attributes(:tv_program => program)     
+      discuss.update_attributes(:tv_program => program)     
       program.update_attributes(:discuss_count => program.discuss_count.next)
-      if quote != nil
-        quote.update_attributes(:tv_program => program)
-        src.update_attributes(:quote_count => src.quote_count.next)      
-        quote.update_attributes(:discuss_type => 1)
-        quote.update_attributes(:src_id => src.id)
-        DiscussRelationship.create(:src => src, :quote => quote, :time => Time.now)
+      if source != nil
+        source.update_attributes(:quote_count => discuss.quote_count.next)      
+        discuss.update_attributes(:discuss_type => 1)
+        discuss.update_attributes(:src_id => source.id)
+        DiscussRelationship.create(:src => source, :quote => discuss, :time => Time.now)
       end
-      src.save
+      discuss.save
     end
   end
 
-  def self.destory_relationship(user, src, quote, program)
-    if user == nil || src == nil || program == nil
+  def self.destory_relationship(user, source, discuss, program)
+    if user == nil || discuss == nil || program == nil
       return
     end
 
-    src.transaction do
+    discuss.transaction do
       user.update_attributes(:discuss_count => (user.discuss_count-1) )
-      src.update_attributes(:tv_program => nil)
+      discuss.update_attributes(:tv_program => nil)
       program.update_attributes(:discuss_count => (program.discuss_count-1) ) 
-      if quote != nil
-        quote.update_attributes(:tv_program => nil)
-        src.update_attributes(:quote_count => (src.quote_count-1) )
-        quote.update_attributes(:discuss_type => 0)
-        quote.update_attributes(:src_id => nil)
-        DiscussRelationship.destory(:src => src, :quote => quote)
+      if source != nil
+        source.update_attributes(:quote_count => (souce.quote_count-1) )
+        discuss.update_attributes(:discuss_type => 0)
+        discuss.update_attributes(:src_id => nil)
+        DiscussRelationship.destory(:src => source, :quote => discuss)
       end
     end
   end
