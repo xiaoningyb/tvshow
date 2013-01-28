823431714cf334ddf0cbe38c8ed7055cf4d38df9
xiaoningyb <xiaoningyb@gmail.com>
2013-01-28 17:40:02 +0800

    added the protection for add user relationship

--- app/controllers/user_relationships_controller.rb
+++ app/controllers/user_relationships_controller.rb
@@ -18,6 +18,10 @@ class UserRelationshipsController < ApplicationController
   end
 
   def self.create_relationship(user, follower)
+    if user.followers.exists?(follower)
+      return
+    end
+
     user.transaction do 
       user.update_attributes(:follower_count => user.follower_count.next)
       user.update_attributes(:version => user.version.next)
@@ -28,6 +32,10 @@ class UserRelationshipsController < ApplicationController
   end
 
   def self.destroy_relationship(user, follower)
+    if !user.followers.exists?(follower)
+      return
+    end
+
     user.transaction do 
       user.update_attributes(:follower_count => (user.follower_count-1) )
       user.update_attributes(:version => user.version.next)
