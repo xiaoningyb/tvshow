a9ed05a52391482373f5a733e88bb66b3f911b20
xiaoningyb <xiaoningyb@gmail.com>
2013-01-29 14:33:49 +0800

    added the test case

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,23 +40,45 @@ cctv.tv_stations << cctv4
 cctv.tv_stations << cctv5
 cctv.tv_stations << cctv6
 
-nba_heat_bull = TvProgram.create(:name => "NBA常规赛:热火vs公牛", :description => "北京时间2013年1月5日常规赛，早上9点至11点")
-nba_spurs_knicks = TvProgram.create(:name => "NBA常规赛:马刺vs尼克斯", :description => "北京时间2013年1月5日常规赛，早上10点至12点")
-xwlb = TvProgram.create(:name => "新闻联播", :description => "新闻联播: 每天晚上7点")
-jjbxs = TvProgram.create(:name => "经济半小时", :description => "经济半小时")
-wyscw = TvProgram.create(:name => "我要上春晚", :description => "我要上春晚")
-gjxw = TvProgram.create(:name => "国际新闻", :description => "国际新闻")
-bxjg = TvProgram.create(:name => "变形金刚3", :description => "电影")
+#add the program
+0.step(24, 1).to_a.each do |i|
+  #for cctv1
+  xwlb = TvProgram.create(:name => "新闻联播(" + i.to_s + ")", :description => "新闻联播: 每天晚上7点")
+  TvProgramship.create(:tv_station => cctv1, :tv_program => xwlb, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600, 
+                       :duration => 60, :is_alive => true)
 
+  #for cctv2
+  jjbxs = TvProgram.create(:name => "经济半小时("+ i.to_s + ")", :description => "经济半小时")
+  TvProgramship.create(:tv_station => cctv2, :tv_program => jjbxs, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600,
+                       :duration => 60, :is_alive => true)
 
-TvProgramship.create(:tv_station => cctv5, :tv_program => nba_heat_bull, :begin => Time.local(2013,1,4,11), :end =>  Time.local(2014,1,4,11), :duration => 120, :is_alive => true)
-TvProgramship.create(:tv_station => cctv5, :tv_program => nba_heat_bull, :begin => Time.local(2013,1,5,19), :end =>  Time.local(2013,1,5,21), :duration => 120, :is_alive => false)
-TvProgramship.create(:tv_station => cctv5, :tv_program => nba_spurs_knicks, :begin => Time.mktime(2013,1,6,21), :end => Time.mktime(2013,1,6,23), :duration => 120, :is_alive => false)
-TvProgramship.create(:tv_station => cctv1, :tv_program => xwlb, :begin => Time.local(2013,1,4,11), :end =>  Time.local(2014,1,4,11), :duration => 120, :is_alive => true)
-TvProgramship.create(:tv_station => cctv2, :tv_program => jjbxs, :begin => Time.local(2013,1,5,11), :end =>  Time.local(2014,1,5,11), :duration => 120, :is_alive => true)
-TvProgramship.create(:tv_station => cctv3, :tv_program => wyscw, :begin => Time.local(2013,1,4,11), :end =>  Time.local(2014,1,4,11), :duration => 120, :is_alive => true)
-TvProgramship.create(:tv_station => cctv4, :tv_program => gjxw, :begin => Time.local(2013,1,4,11), :end =>  Time.local(2014,1,4,11), :duration => 120, :is_alive => true)
-TvProgramship.create(:tv_station => cctv6, :tv_program => bxjg, :begin => Time.local(2013,1,4,11), :end =>  Time.local(2014,1,4,11), :duration => 120, :is_alive => true)
+  #for cctv3
+  wyscw = TvProgram.create(:name => "我要上春晚(" + i.to_s + ")", :description => "我要上春晚")
+  TvProgramship.create(:tv_station => cctv3, :tv_program => wyscw, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600,
+                       :duration => 60, :is_alive => true)
+
+  #for cctv4 
+  gjxw = TvProgram.create(:name => "国际新闻(" + i.to_s + ")", :description => "国际新闻")
+  TvProgramship.create(:tv_station => cctv4, :tv_program => gjxw, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600,
+                       :duration => 60, :is_alive => true)
+
+  #for cctv6
+  bxjg = TvProgram.create(:name => "变形金刚3", :description => "电影")
+  TvProgramship.create(:tv_station => cctv6, :tv_program => bxjg, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600,
+                       :duration => 60, :is_alive => true)
+
+  #for cctv5
+  heat_bull = TvProgram.create(:name => "NBA常规赛:热火vs公牛(" + i.to_s + ")", :description => "北京时间2013年1月5日常规赛，早上9点至11点")
+  heat_bull_re = TvProgram.create(:name => "NBA常规赛重播:热火vs公牛(" + i.to_s + ")", :description => "北京时间2013年1月5日常规赛，早上9点至11点")
+  spurs_knicks = TvProgram.create(:name => "NBA常规赛:马刺vs尼克斯(" + i.to_s + ")", :description => "北京时间2013年1月5日常规赛，早上10点至12点")
+  TvProgramship.create(:tv_station => cctv5, :tv_program => heat_bull, :begin => Time.now + i * 1200, :end =>  Time.now + (i + 1) *1200, 
+                       :duration => 20, :is_alive => true)
+  TvProgramship.create(:tv_station => cctv5, :tv_program => heat_bull_re, :begin => Time.now + i * 1200, :end =>  Time.now + (i + 1) *1200, 
+                       :duration => 20, :is_alive => false)
+  TvProgramship.create(:tv_station => cctv5, :tv_program => spurs_knicks, :begin => Time.now + i * 1200, :end =>  Time.now + (i + 1) *1200, 
+                       :duration => 20, :is_alive => false)
+end
+
 
 # for local 
 local = TvGroup.create(:name => "地方电视台", :description => "各级省市电视台")
@@ -64,16 +86,39 @@ local = TvGroup.create(:name => "地方电视台", :description => "各级省市
 hntv = TvStation.create(:name => "湖南卫视", :description => "湖南省级卫星电视台")
 zjtv = TvStation.create(:name => "浙江卫视", :description => "浙江省级卫星电视台")
 jstv = TvStation.create(:name => "江苏卫视", :description => "江苏省级卫星电视台")
-sgtv = TvStation.create(:name => "东方卫视", :description => "上海市卫星电视台")
+smgtv = TvStation.create(:name => "东方卫视", :description => "上海市卫星电视台")
 wxsport = TvStation.create(:name => "五星体育", :description => "上海文广所属体育体育频道")
 gdsport = TvStation.create(:name => "广东体育", :description => "广东省级电视台所属体育频道")
 
 local.tv_stations << hntv
 local.tv_stations << zjtv
 local.tv_stations << jstv
-local.tv_stations << sgtv
+local.tv_stations << smgtv
 local.tv_stations << wxsport
 local.tv_stations << gdsport
+
+#add the program
+0.step(24, 1).to_a.each do |i|
+  #for hntv
+  klns = TvProgram.create(:name => "快乐女生(" + i.to_s + ")", :description => "选秀比赛")
+  TvProgramship.create(:tv_station => hntv, :tv_program => klns, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600, 
+                       :duration => 60, :is_alive => true)
+
+  #for hntv
+  zghsy = TvProgram.create(:name => "中国好声音(" + i.to_s + ")", :description => "选秀比赛")
+  TvProgramship.create(:tv_station => zjtv , :tv_program => zghsy, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600, 
+                       :duration => 60, :is_alive => true)
+
+  #for jstv
+  fcwr = TvProgram.create(:name => "非诚勿扰(" + i.to_s + ")", :description => "相亲节目")
+  TvProgramship.create(:tv_station => jstv , :tv_program => fcwr, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600, 
+                       :duration => 60, :is_alive => true)
+
+  #for smgtv
+  zgdrx = TvProgram.create(:name => "中国达人秀(" + i.to_s + ")", :description => "比赛节目")
+  TvProgramship.create(:tv_station => smgtv, :tv_program => zgdrx, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600, 
+                       :duration => 60, :is_alive => true)  
+end
 
 # for sports
 sport = TvGroup.create(:name => "体育频道", :description => "体育相关频道")
@@ -82,14 +127,19 @@ sport.tv_stations << cctv5
 sport.tv_stations << wxsport
 sport.tv_stations << gdsport
 
-
-TvProgramship.create(:tv_station => wxsport, :tv_program => nba_heat_bull, :begin => Time.mktime(2013,1,4,9), :end => Time.mktime(2014,1,4,11), :duration => 120, :is_alive => true)
-
-TvProgramship.create(:tv_station => gdsport, :tv_program => nba_heat_bull, :begin => Time.mktime(2013,1,4,21), :end => Time.mktime(2013,1,4,23), :duration => 120, :is_alive => false)
-
+#add the program
+0.step(24, 1).to_a.each do |i|
+  #for wxsport
+  aus_tennis = TvProgram.create(:name => "澳大利亚网球比赛(" + i.to_s + ")", :description => "网球比赛")
+  TvProgramship.create(:tv_station => wxsport, :tv_program => aus_tennis, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600, 
+                       :duration => 60, :is_alive => true)
+  #for hntv
+  eng_football = TvProgram.create(:name => "西班牙足球联赛(" + i.to_s + ")", :description => "足球比赛")
+  TvProgramship.create(:tv_station => gdsport , :tv_program => eng_football, :begin => Time.now + i * 3600, :end =>  Time.now + (i + 1) * 3600, 
+                       :duration => 60, :is_alive => true)
+end
 
 # for user
-
 user_ning = User.create(:name => "xiaoningyb", :password => "123456", :email => "xiaoningyb@gmail.com")
 user_bill = User.create(:name => "bill_tang", :password => "123456", :email => "bill.tang@nebutown.com")
 user_sun = User.create(:name => "孙大发", :password => "123456", :email => "2358340741@qq.com")
@@ -99,15 +149,6 @@ UserRelationshipsController.create_relationship(user_bill, user_ning)
 UserRelationshipsController.create_relationship(user_bill, user_sun)
 UserRelationshipsController.create_relationship(user_ning, user_sun)
 
-
 # for discuss
-
-discuss1 = Discuss.create(:topic => "太垃圾了!", :content => "这个节目太垃圾了，浪费时间!", :user_id => user_ning.id, :time => Time.now)
-discuss2 = Discuss.create(:topic => "awesome", :content => "Incredible!", :user_id => user_bill.id, :time => Time.now)
-discuss3 = Discuss.create(:topic => "我觉得挺好的", :content => "节目不好，妹子漂亮!", :user_id => user_sun.id, :time => Time.now)
-
-DiscussRelationshipsController.create_relationship(user_ning, discuss1, nil, nba_heat_bull)
-DiscussRelationshipsController.create_relationship(user_bill, discuss1, discuss2, nba_heat_bull)
-DiscussRelationshipsController.create_relationship(user_sun, discuss1, discuss3, nba_heat_bull)
 
 
