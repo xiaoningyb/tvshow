2ae057c248e620545afb99761d299bb21f1e7e52
xiaoningyb <xiaoningyb@gmail.com>
2013-01-29 18:08:42 +0800

    added the current auth_token in response header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :send_form_authenticity_token
+
+  private
+  def send_form_authenticity_token
+    response.headers['X-Authenticity-Token'] = form_authenticity_token
+  end
 end
