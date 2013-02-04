22931334f42fe74d38c4dcf8df4d8bc1bea3d89b
xiaoningyb <xiaoningyb@gmail.com>
2013-02-04 12:42:56 +0800

    added the output for tv program

--- app/controllers/tv_programs_controller.rb
+++ app/controllers/tv_programs_controller.rb
@@ -27,12 +27,47 @@ class TvProgramsController < ApplicationController
 
   #show tv group info
   def show
+    if (params[:cmd] != nil) && (params[:cmd] == "detail")
+      show_detail(params)
+    elsif ((params[:cmd] != nil) && (params[:cmd] == "discuss") && (params[:from] != nil) && (params[:to] != nil))
+      show_discusses(params)
+    else
+      show_simple(params)
+    end
+  end
+
+  #show simple tv info
+  def show_simple(params)
     @program = TvProgram.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
       format.xml { render :xml => @program.to_xml }
       format.json { render :json => @program.to_json }
+    end
+  end
+
+  #show detail tv info
+  def show_detail(params)
+    @program = TvProgram.find(params[:id])
+    program_format = format_detail_json(@program, @program.discusses.last(10))
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml { render :xml => program_format.to_xml }
+      format.json { render :json => program_format.to_json}
+    end
+  end
+
+  #show detail tv info
+  def show_discusses(params)
+    @program = TvProgram.find(params[:id])
+    discusses_format = format_discusses_json(@program.discusses[params[:from].to_i .. params[:to].to_i])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml { render :xml => discusses_format.to_xml }
+      format.json { render :json => discusses_format.to_json}
     end
   end
 
@@ -56,4 +91,31 @@ class TvProgramsController < ApplicationController
     
     redirect_to :action => :index
   end
+
+  #format json output function
+  def format_detail_json(program, discusses)    
+    program_format = { :program_id => program.id, :name => program.name, :description => program.description, :episode => program.episode, 
+                       :image => program.image, :key_word => program.key_word, :subscriber_count => program.subscriber_count,
+                       :discuss_count => program.discuss_count, :checkin_count => program.checkin_count }
+    
+    discusses_format = []
+    discusses.each do |discuss|
+      discuss_format = discuss.to_json
+      discusses_format << discuss_format
+    end
+    program_format[:discusses] = discusses_format
+    return program_format
+  end
+
+  #format json output function
+  def format_discusses_json(discusses)    
+    discusses_format = []
+    discusses.each do |discuss|
+      discuss_format = discuss.to_json
+      discusses_format << discuss_format
+    end
+    return discusses_format
+  end
+
+
 end
